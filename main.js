cd4d0aa56ed6bc05b26e0aa78aa2071089196fee
M.Mustafain Haidery <160395846+MustafainHaidery@users.noreply.github.com>
2024-05-08 00:44:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var message = "Thank you saifeee";
+console.log(message);
+// I am practicing on JavaScript & TypeScript
+/*
+I am an AI assistant, designed to provide helpful responses to questions.
+I am trained on a wide range of topics, but for now, I am focused on assisting with coding tasks.
+I don't have personal experiences or feelings, but I'm always happy to help with any questions you may have!
+*/
+var age = 25;
+var life = 50;
---
+++ main.ts
@@ -0,0 +1,12 @@
+let message = "Thank you saifeee";
+console.log(message)
+
+// I am practicing on JavaScript & TypeScript
+
+/*
+I am an AI assistant, designed to provide helpful responses to questions.
+I am trained on a wide range of topics, but for now, I am focused on assisting with coding tasks.
+I don't have personal experiences or feelings, but I'm always happy to help with any questions you may have!
+*/
+let age = 25;   
+let life: number = 50;
