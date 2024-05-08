8af99350d950916e5b4cfb13e1a34c09ce852e82
M.Mustafain Haidery <160395846+MustafainHaidery@users.noreply.github.com>
2024-05-08 18:19:29 +0500

    Add files via upload

---
+++ 2main.ts
@@ -0,0 +1,39 @@
+var num1 = 5;
+var num2 = 10;
+
+var sum = num1 + num2;
+var num3 = 5;  
+var num4 = 5;
+var sum2 = num3 * num4
+
+var Harry = 50;
+var saifee = 100;
+var total = Harry / saifee
+
+var num5 = 10;
+var num6 = 10;
+
+var sum3 = num5 - num6;
+
+
+console.log("Sum: " + sum);
+console.log("Sum2:" + sum2);
+console.log(total);
+console.log(sum3);
+
+let variable: number = 0
+variable = 5
+
+const additionResult: number = +1; 
+console.log(additionResult);
+
+const multiplyResult: number = 10 * 5; 
+console.log(multiplyResult);
+
+const divisionResult: number = 0 / 12;
+console.log(divisionResult);
+
+const subtractionResult: number = 10 - 5;
+console.log(subtractionResult);
+
+
---
+++ simplecal.ts
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+
+function calculator() {
+  inquirer
+    .prompt([
+      {
+        type: 'number',
+        name: 'num1',
+        message: 'Enter the first number:',
+      },
+      {
+        type: 'number',
+        name: 'num2',
+        message: 'Enter the second number:',
+      },
+      {
+        type: 'list',
+        name: 'operation',
+        message: 'Select an operation:',
+        choices: ['+', '-', '*', '/'],
+      },
+    ])
+    .then((answers) => {
+      const { num1, num2, operation } = answers;
+      let result;
+
+      switch (operation) {
+        case '+':
+          result = num1 + num2;
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          result = num1 / num2;
+          break;
+        default:
+          console.log('Invalid operation');
+          return;
+      }
+
+      console.log(`Result: ${num1} ${operation} ${num2} = ${result}`);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+calculator();
+
+
+
